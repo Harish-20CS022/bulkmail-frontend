0c54edd064a03677e2266968da4642ade1cddd95
Harish-CS022 <hariharan.ariyagounden@gmail.com>
2026-06-25 14:32:39 +0530

    axios update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   function send(){
     setstatus(true)
-    axios.post("https://bulkmail-backend-n9s5.onrender.com/", {msg:msg,emailList:emailList}).
+    axios.post("https://bulkmail-backend-n9s5.onrender.com/sendemail", {msg:msg,emailList:emailList}).
     then(function(data){
       if(data.data === true){
         alert("Email Send Successfully")
